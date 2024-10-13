7f50171bcde49fe2b16962cf4199930633c06104
albrtomca <therealalbertomeca@gmail.com>
2024-10-13 19:54:45 +0200

    Comienzo de lectura
    Co-authored-by: IzmaelMG <IzmaelMG@users.noreply.github.com>

--- exec_lines.c
+++ exec_lines.c
@@ -4,6 +4,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <string.h>
+#include <fcntl.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <sys/wait.h>
 
 #define BUF_SIZE_DEF 16
 #define MAX_LINE_SIZE_DEF 32
@@ -13,12 +18,18 @@
 #define LINE_MIN 16
 #define LINE_MAX 1024
 
+
 int main(int argc, char **argv)
 {
+    pid_t pid; /* Usado en el proceso padre para guardar el PID del proceso hijo */
+    int fd; //Descriptor de fichero
     int opt;
     int buf_size = BUF_SIZE_DEF, max_line_size = MAX_LINE_SIZE_DEF;
+
+    char *buffer;
     
     optind = 1;
+    
     while ((opt = getopt(argc, argv, "b:l:h")) != -1)
     {
         switch (opt)
@@ -58,11 +69,81 @@ int main(int argc, char **argv)
         fprintf(stderr, "Error: El tamaño de línea tiene que estar entre 16 y 1024.\n");
     }
 
+    if ((buffer = (char *) malloc(buf_size * sizeof(char))) == NULL)
+    {
+        perror("malloc()");
+        exit(EXIT_FAILURE);
+    }
 
-    // printf("flag: %d, n: %d, s: \"%s\"", flag, n, s);
-    // for (int i = optind; i < argc; i++)
-    //     printf(", param%d: \"%s\"", i - optind, argv[i]);
-    // printf("\n");
+    // ssize_t bytes_leidos;
+        
+    // while((bytes_leidos = read(STDIN_FILENO, buffer, buf_size)) > 0)
+    // {
+    //     buffer[bytes_leidos] = '\0'; //preguntar si eso esta bien
+    //     if(bytes_leidos == -1)
+    //     {
+    //         perror("read()");
+    //         exit(EXIT_FAILURE);
+    //     }
 
-    return EXIT_SUCCESS;
+    //     printf("Numero de bytes leidos: %ld\n", bytes_leidos);
+    //     printf("Buffer: %s\n", buffer);
+
+    //     char *linea = strtok(buffer, "\n");
+    //     char *resto = strtok(NULL, "");
+    //     printf("Linea: %s\n", linea);
+    //     printf("Resto: %s\n", resto);
+
+    // } 
+    
+
+
+
+
+
+    ssize_t bytes_leidos;
+    //buffer[buf_size + 1]; // Espacio adicional para el terminador nulo
+    buffer[0] = '\0';          // Inicia el buffer vacío
+    size_t offset = 0;         // Inicio para la siguiente lectura
+
+    while ((bytes_leidos = read(STDIN_FILENO, buffer + offset, buf_size - offset)) > 0) {
+        if (bytes_leidos == -1) {
+            perror("read()");
+            exit(EXIT_FAILURE);
+        }
+
+        // Agregar el terminador nulo al final de los datos leídos
+        buffer[offset + bytes_leidos] = '\0';
+
+        // Procesar el contenido del buffer
+        char *linea = strtok(buffer, "\n");
+        char *resto = strtok(NULL, "");
+
+        while (linea != NULL) {
+            printf("Linea: %s\n", linea);
+            linea = strtok(NULL, "\n");
+        }
+
+        // Mover el resto al inicio del buffer para la siguiente iteración
+        if (resto != NULL) {
+            offset = strlen(resto);
+            memmove(buffer, resto, offset);
+        } else {
+            offset = 0;  // No hay resto, comenzamos desde el inicio
+        }
+
+        printf("Numero de bytes leidos: %ld\n", bytes_leidos);
+        printf("Buffer para la siguiente iteración: %s\n", buffer);
+    }
+
+    if (bytes_leidos == -1) {
+        perror("read()");
+        exit(EXIT_FAILURE);
+    }
+
+
+
+
+    free(buffer);
+    exit(EXIT_SUCCESS);
 }
